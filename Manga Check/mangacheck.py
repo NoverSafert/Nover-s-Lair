4a50b7a2078a4e48690bb261ee939d50ff162e3b
José Damián <enrique.damian@hotmail.com>
2020-03-09 11:51:38 -0600

    Creación del xlsx

--- Manga Check/mangacheck.py
+++ Manga Check/mangacheck.py
@@ -1,13 +1,21 @@
 #THIS PROGRAM WAS CREATED BY NOVER SAFERT
 
 import logging
-import openpyxl
+from openpyxl import Workbook 
 import webbrowser
+wb = Workbook()
 
-
-def xls_open():
-
+def xlsx_loader():
+    ws = wb.active
+    ws = wb.create_sheet("MySheet")
+    ws.title = "Mangas"
+    ws.sheet_properties.tabColor = "1072BA"
+    logging.debug('xlsx loaded successfully')
+    wb.save("Mangacheck.xlsx")
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
     logging.debug('Start of program')
+    xlsx_loader()
+
+main()
