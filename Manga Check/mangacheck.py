75f95d3cc441c9c35e2e3cfe2d157d1f12b31d23
José Damián <enrique.damian@hotmail.com>
2020-05-16 19:20:57 -0500

    acomodo de funciones

--- Manga Check/mangacheck.py
+++ Manga Check/mangacheck.py
@@ -12,15 +12,28 @@ def xlsx_loader():
     logging.debug( wb1.sheetnames)
     return (wb1)
 
-def webcheck():
-
-
-def MangaAdder():
-
+def checkall(ws,size):
+    size = size + 2
+    for num in range(2, size):
+        cell = "A" + str(num)
+        print(ws[cell].value)
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
     logging.debug('Start of program')
     wb1 = xlsx_loader()
+    ws = wb1['Mangas']
+    option = 0
+    size = 12
+    while (option !=  -1):
+        print(" [1] Check all manga\n [2] Add a manga")
+        print(" [3] Edit manga info \n [-1] Exit\n")
+        option = int(input("Select the number of the option you want\n"))
+        if (option == 1):
+            checkall(ws, size)
+            print("Do you want to any thing else?")
+            op2 = str(input("[Y] [N]\n"))
+            if (op2 == "N" or "n"):
+                break
 
 main()
